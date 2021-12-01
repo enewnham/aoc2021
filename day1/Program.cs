dec91a19df83742e60e64795ccddcf3a69b5d167
Elliot Newnham <enewnham@mri-inc.net>
2021-12-01 11:20:05 -0500

    Add day1 part2

--- day1/Program.cs
+++ day1/Program.cs
@@ -1,8 +1,9 @@
-﻿var depths = await File.ReadAllLinesAsync("input.txt");
+﻿var lines = await File.ReadAllLinesAsync("input.txt");
+var depths = lines.Select(int.Parse).ToList();
 
 var previousDepth = int.MaxValue;
 var increases = 0;
-foreach (var d in depths.Select(l => int.Parse(l)))
+foreach (var d in depths)
 {
     if (d > previousDepth)
         increases++;
@@ -10,3 +11,17 @@ foreach (var d in depths.Select(l => int.Parse(l)))
 }
 
 Console.WriteLine($"Increases={increases}");
+
+previousDepth = int.MaxValue;
+increases = 0;
+for (var ii = 0; ii < depths.Count - 2; ii++)
+{
+    var window = depths.Skip(ii).Take(3);
+    var sum = window.Sum();
+
+    if (sum > previousDepth)
+        increases++;
+    previousDepth = sum;
+}
+
+Console.WriteLine($"Sum Increases={increases}");
