7a20223f5143a4280017ed20f98fc8bc1bb10632
Elliot Newnham <enewnham@mri-inc.net>
2021-12-01 11:08:36 -0500

    Implement day1 part1

---
+++ day1/Program.cs
@@ -0,0 +1,12 @@
+﻿var depths = await File.ReadAllLinesAsync("input.txt");
+
+var previousDepth = int.MaxValue;
+var increases = 0;
+foreach (var d in depths.Select(l => int.Parse(l)))
+{
+    if (d > previousDepth)
+        increases++;
+    previousDepth = d;
+}
+
+Console.WriteLine($"Increases={increases}");
